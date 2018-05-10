6dbe0072d8b29bc24a10dfed7c1616b22bbf6e8a
nillei <309012952@qq.com>
2018-05-10 11:53:37 +0800

    Update run_me_0.sh

--- code/run_me_0.sh
+++ code/run_me_0.sh
@@ -1,1 +1,9 @@
-#! /bin/bash -e# echo "filter html"cd data/python ../code/tools.py -m filter -s News_info_train_example100.txt -t News_info_train_example100_filter.txtpython ../code/tools.py -m filter -s News_info_train.txt -t News_info_train_filter.txtpython ../code/tools.py -m filter -s News_info_validate.txt -t News_info_validate_filter.txt
+#! /bin/bash -e
+
+# echo "filter html"
+
+cd data/
+
+python ../code/tools.py -m filter -s News_info_train_example100.txt -t News_info_train_example100_filter.txt
+python ../code/tools.py -m filter -s News_info_train.txt -t News_info_train_filter.txt
+python ../code/tools.py -m filter -s News_info_validate.txt -t News_info_validate_filter.txt
